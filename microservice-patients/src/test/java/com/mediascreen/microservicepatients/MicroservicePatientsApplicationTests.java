f2f636cac18f742c6b953fc1d2585ef30ab6e30d
Elien <66647337+elienFR@users.noreply.github.com>
2022-06-25 14:16:43 +0200

    init commit

---
+++ microservice-patients/src/test/java/com/mediascreen/microservicepatients/MicroservicePatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediascreen.microservicepatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
