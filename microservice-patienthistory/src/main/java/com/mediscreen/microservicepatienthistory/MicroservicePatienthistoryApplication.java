e90425a59ab5a8782eef145bbadcdf01d0797c23
Elien <66647337+elienFR@users.noreply.github.com>
2022-07-02 17:57:12 +0200

    init microservice-patienthistory

---
+++ microservice-patienthistory/src/main/java/com/mediscreen/microservicepatienthistory/MicroservicePatienthistoryApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservicepatienthistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePatienthistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePatienthistoryApplication.class, args);
+	}
+
+}
---
+++ microservice-patienthistory/src/test/java/com/mediscreen/microservicepatienthistory/MicroservicePatienthistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.microservicepatienthistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePatienthistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
