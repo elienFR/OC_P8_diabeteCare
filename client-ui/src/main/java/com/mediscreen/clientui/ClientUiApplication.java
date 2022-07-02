e093e4092824b5acf5f643a02d6d288a13859a84
Elien <66647337+elienFR@users.noreply.github.com>
2022-07-02 17:56:54 +0200

    externalize config of client-ui

--- client-ui/src/main/java/com/mediscreen/clientui/ClientUiApplication.java
+++ client-ui/src/main/java/com/mediscreen/clientui/ClientUiApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients("com.mediscreen.clientui")
 @EnableDiscoveryClient
 public class ClientUiApplication {
-// TODO : externalize conf file through bootstrap.yml
   @Autowired
   private PatientService patientService;
 
